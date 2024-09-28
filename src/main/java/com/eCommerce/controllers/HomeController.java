0afa385b2f2029c4010f886a3f0963d2088326f7
amos <amosmwololo02@gmail.com>
2024-09-27 23:18:25 -0700

    Initial commit. Starting modules implemented

---
+++ src/main/java/com/eCommerce/controllers/HomeController.java
@@ -0,0 +1,43 @@
+package com.eCommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+		
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "login";
+		
+	}
+	
+	@GetMapping("/register")
+	public String register() {
+		
+		return "register";
+		
+	}
+	
+	@GetMapping("/products")
+	public String product() {
+		
+		return "product";
+		
+	}
+	
+	@GetMapping("/view-product")
+	public String productDetails() {
+		
+		return "view-product";
+		
+	}
+}
