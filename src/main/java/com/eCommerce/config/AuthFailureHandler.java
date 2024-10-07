6288acfc2fc6a70fec5392184b63b4102dc08fee
amos <amosmwololo02@gmail.com>
2024-10-07 12:35:51 -0700

    Implemented AccountNonLocking and login attempt limit module

---
+++ src/main/java/com/eCommerce/config/AuthFailureHandler.java
@@ -0,0 +1,67 @@
+package com.eCommerce.config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import com.eCommerce.model.User;
+import com.eCommerce.repository.UserRepository;
+import com.eCommerce.service.UserService;
+import com.eCommerce.utils.AppConstraint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler{
+	
+	@	Autowired
+	private UserRepository userRepository;
+	
+	@	Autowired
+	private UserService userService;
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+
+
+		String email = request.getParameter("username");
+		
+		User user = userRepository.findByEmail(email);
+		
+		if(user.getIsEnabled()) {
+			
+			if(user.getAccountNonLocked()) {
+				
+				if(user.getFailAttempts() < AppConstraint.ATTEMPT_LIMIT) {
+					
+					userService.increaseFailAttempts(user);
+				}else {
+					userService.userAccountLock(user);
+					exception = new LockedException("your account is locked. You have reached your Failed attempts limit");
+				}
+				
+			}else {
+				
+				if(userService.unlockTimeExpired(user)) {
+					exception = new LockedException("your account is unlocked!! Please login");
+				}else {
+				exception = new LockedException("your account is locked!! Please try again  later");
+				}
+			}
+		}else {
+			exception = new LockedException("your account is inactive");
+		}
+		
+		super.setDefaultFailureUrl("/signin?error");
+		super.onAuthenticationFailure(request, response, exception);
+	}
+
+	
+}
---
+++ src/main/java/com/eCommerce/utils/AppConstraint.java
@@ -0,0 +1,7 @@
+package com.eCommerce.utils;
+
+public class AppConstraint {
+
+	public static final long UNLOCK_DURATION_TIME = 1*60 *60 *1000;
+	public static final Integer ATTEMPT_LIMIT = 3;
+}
